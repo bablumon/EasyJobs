e8ab6f83392deca16b7083ce7e697fa1423dadaa
sreenathm99 <sreenathmenon97@gmail.com>
2017-10-20 20:27:41 +0530

    link extracted

--- app/src/main/java/me/toptas/rssreader/main/contents.java
+++ app/src/main/java/me/toptas/rssreader/main/contents.java
@@ -3,98 +3,70 @@ package me.toptas.rssreader.main;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.apache.http.protocol.HTTP;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
 
 import me.toptas.rssreader.R;
 import me.toptas.rssreader.model.RssItem;
 
 public class contents extends AppCompatActivity {
     static String jim;
+    TextView outputTextView;
     @Override
-     protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contents);
-
-
-
-        GetContents gc=new GetContents();
-        gc.execute(jim,"a=b");
+        outputTextView = (TextView) findViewById(R.id.textView2);
+        new doit().execute();
     }
+    public  class doit extends AsyncTask<Void,Void,Void> {
+
+        String parsed;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document doc = Jsoup.connect("jim").get();
+
+                Elements elementsHtml = doc.getElementsByAttributeValue("class", "app");
+
+                for (Element element : elementsHtml){
+                    Log.i("PARSED ELEMENTS:", URLDecoder.decode(element.text(), HTTP.UTF_8));
+                    parsed=URLDecoder.decode(element.text(), HTTP.UTF_8);
+
+
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid){
+            super.onPostExecute(aVoid);
+            outputTextView.setText(parsed);
+        }
+    }
+
+
     public static void urlstrng(String jack){
         jim=jack;
-    }
-
-
-
-
-    private  class GetContents extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... p) {
-            String targetURL = p[0];
-            String urlParameters = p[1];
-            URL url;
-            HttpURLConnection connection = null;
-            try {
-                //Create connection
-                url = new URL(targetURL);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type",
-                        "application/x-www-form-urlencoded");
-
-                connection.setRequestProperty("Content-Length", "" +
-                        Integer.toString(urlParameters.getBytes().length));
-                connection.setRequestProperty("Content-Language", "en-US");
-
-                connection.setUseCaches(false);
-                connection.setDoInput(true);
-                connection.setDoOutput(true);
-
-                //Send request
-                DataOutputStream wr = new DataOutputStream(
-                        connection.getOutputStream());
-                wr.writeBytes(urlParameters);
-                wr.flush();
-                wr.close();
-
-                //Get Response
-                InputStream is = connection.getInputStream();
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-                String line;
-                StringBuffer response = new StringBuffer();
-                while ((line = rd.readLine()) != null) {
-                    response.append(line);
-                    response.append('\r');
-                }
-                rd.close();
-                return response.toString();
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-                return null;
-
-            } finally {
-
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-
-
-        }
-
-        protected void onPostExecute(String result) {
-
-            //TextView awesome=(TextView)findViewById(R.id.textView);
-            //awesome.setText(result);
-
-        }
     }
 }
